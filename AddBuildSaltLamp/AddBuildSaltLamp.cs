f98f80d01d8959e0e19358b505d62874a06873fd
Chasel <fgc0109@hotmail.com>
2018-11-15 06:13:37 +0800

    添加一个台灯

---
+++ AddBuildSaltLamp/AddBuildSaltLamp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using TUNING;
+using UnityEngine;
+
+public class AddBuildSaltLamp : IBuildingConfig
+{
+    public override BuildingDef CreateBuildingDef()
+    {
+        string id = "AddBuildSaltLamp";
+        int width = 1;
+        int height = 2;
+        string anim = "saltlamp_kanim";
+        int hitpoints = 10;
+        float construction_time = 10f;
+        float[] tier = BUILDINGS.CONSTRUCTION_MASS_KG.TIER1;
+        string[] all_METALS = MATERIALS.ALL_METALS;
+        float melting_point = 800f;
+        BuildLocationRule build_location_rule = BuildLocationRule.OnFloor;
+        EffectorValues none = NOISE_POLLUTION.NONE;
+        BuildingDef buildingDef = BuildingTemplates.CreateBuildingDef(id, width, height, anim, hitpoints, construction_time, tier, all_METALS, melting_point, build_location_rule, BUILDINGS.DECOR.NONE, none, 0.2f);
+        buildingDef.RequiresPowerInput = true;
+        buildingDef.EnergyConsumptionWhenActive = 8f;
+        buildingDef.SelfHeatKilowattsWhenActive = 0.5f;
+        buildingDef.ViewMode = SimViewMode.Light;
+        buildingDef.AudioCategory = "Metal";
+        return buildingDef;
+    }
+
+    public override void DoPostConfigurePreview(BuildingDef def, GameObject go)
+    {
+        LightShapePreview lightShapePreview = go.AddComponent<LightShapePreview>();
+        lightShapePreview.lux = 1000;
+        lightShapePreview.radius = 4f;
+        lightShapePreview.shape = LightShape.Circle;
+        lightShapePreview.offset = new CellOffset((int)def.BuildingComplete.GetComponent<Light2D>().Offset.x, (int)def.BuildingComplete.GetComponent<Light2D>().Offset.y);
+    }
+
+    public override void DoPostConfigureComplete(GameObject go)
+    {
+        go.AddOrGet<EnergyConsumer>();
+        go.AddOrGet<LoopingSounds>();
+        Light2D light2D = go.AddOrGet<Light2D>();
+        light2D.overlayColour = LIGHT2D.FLOORLAMP_OVERLAYCOLOR;
+        light2D.Color = LIGHT2D.FLOORLAMP_COLOR;
+        light2D.Range = 4f;
+        light2D.Angle = 0f;
+        light2D.Direction = LIGHT2D.FLOORLAMP_DIRECTION;
+        light2D.Offset = LIGHT2D.FLOORLAMP_OFFSET;
+        light2D.shape = LightShape.Circle;
+        light2D.drawOverlay = true;
+        go.AddOrGetDef<LightController.Def>();
+    }
+
+    public const string ID = "AddBuildSaltLamp";
+}
---
+++ AddBuildSaltLamp/AddBuildSaltLampMod.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Harmony;
+using UnityEngine;
+
+namespace OxygenNotIncludedModDev
+{
+    /// <summary>
+    /// 向BUILDINGS注入数据
+    /// </summary>
+    [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+    internal class Patches_GeneratedBuildings_LoadGeneratedBuildings
+    {
+        private static void Prefix()
+        {
+            Debug.Log(" === GeneratedBuildings.LoadGeneratedBuildings Prefix === " + AddBuildSaltLamp.ID);
+
+            Strings.Add($"STRINGS.BUILDINGS.PREFABS.{nameof(AddBuildSaltLamp).ToUpper()}.NAME", Strings.Get($"STRINGS.BUILDINGS.PREFABS.{"FloorLamp".ToUpper()}.NAME") + "II");
+            Strings.Add($"STRINGS.BUILDINGS.PREFABS.{nameof(AddBuildSaltLamp).ToUpper()}.DESC", Strings.Get($"STRINGS.BUILDINGS.PREFABS.{"FloorLamp".ToUpper()}.DESC"));
+            Strings.Add($"STRINGS.BUILDINGS.PREFABS.{nameof(AddBuildSaltLamp).ToUpper()}.EFFECT", Strings.Get($"STRINGS.BUILDINGS.PREFABS.{"FloorLamp".ToUpper()}.EFFECT"));
+
+            List<string> category = (List<string>)TUNING.BUILDINGS.PLANORDER.First(plan => plan.category == PlanScreen.PlanCategory.Furniture).data;
+            category.Add(AddBuildSaltLamp.ID);
+        }
+
+        //private static void Postfix()
+        //{
+        //    Debug.Log(" === GeneratedBuildings.LoadGeneratedBuildings Postfix === " + AddBuildSaltLamp.ID);
+
+        //    object obj = Activator.CreateInstance(typeof(AddBuildSaltLamp));
+        //    BuildingConfigManager.Instance.RegisterBuilding(obj as IBuildingConfig);
+        //}
+    }
+
+    /// <summary>
+    /// 向TECH_GROUPING注入数据
+    /// </summary>
+    [HarmonyPatch(typeof(Db), "Initialize")]
+    internal class InverseElectrolyzerTechMod
+    {
+        private static void Prefix()
+        {
+            Debug.Log(" === Db.Initialize loaded === " + AddBuildSaltLamp.ID);
+
+            List<string> tech = new List<string>(Database.Techs.TECH_GROUPING["FineDining"]) { AddBuildSaltLamp.ID };
+            Database.Techs.TECH_GROUPING["FineDining"] = tech.ToArray();
+        }
+    }
+}
